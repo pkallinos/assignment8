a99aeb7db2a50309f47ef38c9e59943411635d2f
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-11-11 17:47:48 -0500

    IA: Set up Index page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+		<h1></h1>
+		<form>
+			first
+			last
+			email
+			checkboxes
+			radio
+			text area
+			submit
+			thank you popup
+		</form>
+		<p></p>
+
+
+</body>
+</html>
