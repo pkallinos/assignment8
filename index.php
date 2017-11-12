185611ee0fdc9fa329c9b9217436cbca1a5ccd78
mbushoy27 <32023622+mbushoy27@users.noreply.github.com>
2017-11-12 18:00:08 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -3,24 +3,38 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <title>Linkin Park</title>
     </head>
+
     <body>
-        <h1>Linkin Park</h1>
-
+        <h1>Linkin Park</h1><br>
+<!-- Bio -->
         <h2>Bio</h2>
+        <div class="row align-items-center">
+        <div class="column col-md-4 col-sm-4 col-xs-12">
         <!-- Image from https://blackgirlnerds.com/linkin-park-meant-depressed-black-girl-nerd/ -->
-        <img src="images/band.jpg" alt="Linkin_Park">
+        <img src="images/band.jpg" class="img-responsive img-circle" alt="Linkin_Park">
+        </div>
 
+        <div class="column col-md-8 col-sm-8 col-xs-12">
         <p>Linkin Park is an American rock band.They formed in 1996, in the Los Angeles suburb of Agoura Hills, around the core of high school friends Mike Shinoda, Brad Delson, and Rob Bourdon. Joe Hahn and Dave Farrell soon joined, as well as a singer named Mark Wakefield, who left by 1999, when the group was called Hybrid Theory. He was replaced by Chester Bennington, an Arizonan who'd been in a group called Grey Daze.</p>
+        </div>
+        </div>
+<!-- End Bio -->
 
-        <h2>Album</h2>
+<!-- Albums -->
+        <br><h2>Albums</h2>
 
-        <h3>Hybrid Theory</h3>
-
+<!-- Hybrid Theory -->
+        <div class="row align-items-center">
+        <div class="column col-md-4 col-sm-4 col-xs-12">
         <!-- Image from https://en.wikipedia.org/wiki/Hybrid_Theory -->
         <img src="images/hybrid_theory.jpg" alt="Hybrid_Thoery_Art">
+        </div>
 
+        <div class="column col-md-8 col-sm-8 col-xs-12">
+        <h3>Hybrid Theory</h3>
         <p>Hybrid Theory is the debut studio album by American rock band Linkin Park, released on October 24, 2000 through Warner Bros. Records. As of 2017, the album has been certified diamond by the RIAA for sales in the United States of over eleven million units, and peaking at number two on the US Billboard 200.</p>
 
         <h4>Singles</h4>
@@ -29,12 +43,21 @@
             <li>"Crawling"</li>
             <li>"In the End"</li>
         </ul>
+        </div>
+        </div>
+        <br>
+<!-- End Hybrid Theory -->
 
-        <h3>Meteora</h3>
+<!-- Meteora -->
+        <div class="row align-items-center">
+        <div class="column col-md-4 col-sm-4 col-xs-12">
 
         <!-- Image from https://en.wikipedia.org/wiki/Meteora_(album) -->
         <img src="images/meteora.jpg" alt="Meteora_Art">
+        </div>
 
+        <div class="column col-md-8 col-sm-8 col-xs-12">
+        <h3>Meteora</h3>
         <p>Meteora is the second studio album by American rock band Linkin Park. It was released on March 25, 2003 through Warner Bros. Records. The album was produced by the band alongside Don Gilmore.</p>
 
         <h4>Singles</h4>
@@ -43,12 +66,21 @@
             <li>"Faint"</li>
             <li>"Numb"</li>
         </ul>
+        </div>
+        </div>
+        <br>
+<!-- End Meteora  -->
 
-        <h3>Minutes to Midnight</h3>
+<!-- Minutes to Midnight -->
+        <div class="row align-items-center">
+        <div class="column col-md-4 col-sm-4 col-xs-12">
 
         <!-- Image From https://en.wikipedia.org/wiki/Minutes_to_Midnight_(Linkin_Park_album) -->
         <img src="images/minutes_to_midnight.jpg" alt="Minutes_to_Midnight_Art">
+        </div>
 
+        <div class="column col-md-8 col-sm-8 col-xs-12">
+        <h3>Minutes to Midnight</h3>
         <p>Minutes to Midnight is the third studio album by American rock band Linkin Park, released on May 14, 2007, through Warner Bros. Records. The album was produced by Mike Shinoda and Rick Rubin.</p>
 
         <h4>Singles</h4>
@@ -57,12 +89,21 @@
             <li>"Bleed It Out"</li>
             <li>"Shadow of the Day"</li>
         </ul>
+        </div>
+        </div>
+        <br>
+<!-- End Minutes to Midnight -->
 
-        <h3>A Thousand Suns</h3>
+<!-- A Thousand Suns -->
+        <div class="row align-items-center">
+        <div class="column col-md-4 col-sm-4 col-xs-12">
 
         <!-- Image From https://en.wikipedia.org/wiki/A_Thousand_Suns -->
         <img src="images/a_thousand_suns.jpg" alt="A_Thousand_Suns_Art">
+        </div>
 
+        <div class="column col-md-8 col-sm-8 col-xs-12">
+        <h3>A Thousand Suns</h3>
         <p>A Thousand Suns is the fourth studio album by American rock band Linkin Park. It was released on September 8, 2010, by Warner Bros. Records. The album was written by the band and was produced by Linkin Park vocalist Mike Shinoda and Rick Rubin, who worked together to produce the band's previous studio album Minutes to Midnight (2007).</p>
 
         <h4>Singles</h4>
@@ -71,12 +112,21 @@
             <li>"Waiting for the End"</li>
             <li>"Burning in the Skies"</li>
         </ul>
+        </div>
+        </div>
+        <br>
+<!-- End A Thousand Suns -->
 
-        <h3>Living Things</h3>
+<!-- Living Things -->
+        <div class="row align-items-center">
+        <div class="column col-md-4 col-sm-4 col-xs-12">
 
         <!-- Image From https://en.wikipedia.org/wiki/Living_Things_(Linkin_Park_album) -->
         <img src="images/living_things" alt="Living_Things_Art">
+        </div>
 
+        <div class="column col-md-8 col-sm-8 col-xs-12">
+        <h3>Living Things</h3>
         <p>Living Things is the fifth studio album by American rock band Linkin Park. It was released under Warner Bros. Records and Machine Shop Recordings on June 20, 2012, in Japan, and throughout the rest of the world during the following week. Production was handled by vocalist Mike Shinoda and Rick Rubin.</p>
 
         <h4>Singles</h4>
@@ -85,12 +135,21 @@
             <li>"Lost in the Echo"</li>
             <li>"Powerless"</li>
         </ul>
+        </div>
+        </div>
+        <br>
+<!-- End Living Things -->
 
-        <h3>The Hunting Party</h3>
-
+<!-- The Hunting Party -->
+        
+        <div class="row align-items-center">
+        <div class="column col-md-4 col-sm-4 col-xs-12">
         <!-- Image From https://en.wikipedia.org/wiki/The_Hunting_Party_(album) -->
         <img src="images/the_hunting_party.jpg" alt="The_Hutning_Party_Art">
+        </div>
 
+        <div class="column col-md-8 col-sm-8 col-xs-12">
+        <h3>The Hunting Party</h3>
         <p>The Hunting Party is the sixth studio album by American rock band Linkin Park. The album, produced by band members Mike Shinoda and Brad Delson, was released by Warner Bros. Records and Machine Shop on June 13, 2014. It is the first album since Meteora (2003) not to be produced with Rick Rubin, after producing the band's previous three studio albums.</p>
 
         <h4>Singles</h4>
@@ -99,12 +158,21 @@
             <li>"Until It's Gone"</li>
             <li>"Wastelands"</li>
         </ul>
+        </div>
+        </div>
+        <br>
+<!-- End The Hunting Party -->
 
-        <h3>One More Light</h3>
+<!-- One More Light -->
 
+        <div class="row align-items-center">
+        <div class="column col-md-4 col-sm-4 col-xs-12">
         <!-- Image From https://en.wikipedia.org/wiki/One_More_Light -->
         <img src="images/one_more_light.jpg" alt="One_More_Light_Art">
+        </div>
 
+        <div class="column col-md-8 col-sm-8 col-xs-12">
+        <h3>One More Light</h3>
         <p>One More Light is the seventh studio album by American rock band Linkin Park. It was released on May 19, 2017 through Warner Bros. and Machine Shop. It is the last Linkin Park record produced before lead vocalist Chester Bennington's death on July 20, 2017.</p>
 
         <h4>Singles</h4>
@@ -113,27 +181,36 @@
             <li>"Talking to Myself"</li>
             <li>"One More Light"</li>
         </ul>
+        </div>
+        </div>
+        <br>
+<!-- End One More Light -->
 
-
+<!-- Start Form -->
         <form method="POST" action="survey-processor.php">
 
+<!-- Basic Info -->
+
+            <div class="form-group">
             <h2>Basic Information</h2>
 
             <label for="name">First Name:</label>
-            <input type="text" name="firstname" id="firstname">
+            <input type="text" class="form-control" name="firstname" id="firstname">
             <br>
             <br>
 
             <label for="name">Last Name:</label>
-            <input type="text" name="lastname" id="lastname">
+            <input type="text" class="form-control"  name="lastname" id="lastname">
             <br>
             <br>
 
             <label for="email">Email:</label>
-            <input type="email" name="email" id="email">
-
+            <input type="email" class="form-control" name="email" id="email">
+            </div>
             <br>
+<!-- End Basic Info -->
 
+<!-- Favorite Album -->
             <fieldset>
                 <h2>Which Linkin Park album is your favorite?</h2>
 
@@ -171,7 +248,9 @@
                 <label for="light-radio">One More Light</label>
                 <br>
                 <br>
+<!-- End Favorite Album -->
 
+<!-- Songs Section -->
                 <h2>Which Linkin Park songs do you like?</h2>
 
                 <input type="checkbox" name="crawling-check" id="crawling-check" value="crawling">
@@ -198,7 +277,9 @@
                 <label for="what-check">What I've Done</label>
                 <br>
                 <br>
+<!-- End Songs -->
 
+<!-- Favorite Song -->
                 <h2>Which song is your favorite?</h2>
 
                 <input type="radio" name="favorite-song" id="crawling-radio" value="crawling">
@@ -226,17 +307,20 @@
                 <br>
                 <br>
 
+                <div class="form-group">
                 <label for="message">Why is this song your favorite?</label>
                 <br>
-                <textarea name="message" id="message"></textarea>
+                <textarea name="message" class="form-control" id="message"></textarea>
                 <br>
-                <br>
+<!-- End Favorite Songs -->
 
             </fieldset>
 
-            <input type="submit" name="submit" id="submit" value="Submit">
+<!-- Submit Button -->
+            <input type="submit" name="submit" class="btn btn-primary" id="submit" value="Submit"> <br><br>
 
         </form>
+<!-- End Form -->
 
         <!-- site source of our info -->
         <footer>
@@ -254,5 +338,9 @@
 
         </footer>
 
-    </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>        
+
+</body>
 </html>
