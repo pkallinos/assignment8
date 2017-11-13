a2d96a4bc8e37fafa715e1fe7224ba1500eccc05
dbloom4 <31747138+dbloom4@users.noreply.github.com>
2017-11-13 13:19:57 -0500

    Update survey-processor.php

--- survey-processor.php
+++ survey-processor.php
@@ -1,11 +1,38 @@
 <?php
 
 	$dbhost = "localhost";
-	$dbuser = "";
-	$dbpass = "";
-	$dbname = "";
+	$dbuser = "urcscon3_shangha";
+	$dbpass = "coffee1N";
+	$dbname = "urcscon3_shanghai";
 
 	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+
+// Often these are form values in $_POST
+	$fname = Trim(stripslashes($_POST['first']));
+	$lname = Trim(stripslashes($_POST['last']));
+	$email = Trim(stripslashes($_POST['email']));
+	$album = Trim(stripslashes($_POST['favorite-album']));
+	$song = Trim(stripslashes($_POST['song']));
+	$favsong = Trim(stripslashes($_POST['favorite-song']));
+	
+	// Escape all strings
+	$fname = mysqli_real_escape_string($connection, $fname);
+	$lname = mysqli_real_escape_string($connection, $lname);
+	$email = mysqli_real_escape_string($connection, $email);
+	$album = mysqli_real_escape_string($connection, $album);
+	$song = mysqli_real_escape_string($connection, $song);
+	$favsong = mysqli_real_escape_string($connection, $favsong);
+	
+
+	// 2. Perform database query
+	$query  = "INSERT INTO survey (";
+	$query .= "  fname, lname, email, album, song, favsong";
+	$query .= ") VALUES (";
+	$query .= "  '{$fname}', '{$lname}', '{$email}', '{$album}', '{$song}', '{$favsong}' ";
+	$query .= ")";
+
+	$result = mysqli_query($connection, $query);
 
 ?>
 
@@ -17,7 +44,7 @@
 <body>
 
 	<h1>Status</h1>
-    <h2>Thank you <?php echo $_POST["firstname"]; ?> for your response!</h2>
+    <h2>Thank you <?php echo $_POST["first"]; ?> for your response!</h2>
     <a href="index.php">Return back to survery</a>
 
 <?php
@@ -33,6 +60,7 @@
 		die("Failed to record answers. Please try again");
 	}
 ?>
+	<a href="admin.php">Content</a>
 
 </body>
 </html>
