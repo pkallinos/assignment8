d4b45010408851ccf78e8049ef2e1e87c14baac8
pkallinos <31747117+pkallinos@users.noreply.github.com>
2017-11-13 15:51:29 -0500

    IA: update admin

--- admin-update.php
+++ admin-update.php
@@ -10,7 +10,7 @@
 	$updatedField1 = Trim(stripslashes($_POST['field1']));
 	$updatedField2 =  Trim(stripslashes($_POST['field2']));
 
-	$query  = "UPDATE mytable SET ";
+	$query  = "UPDATE survey SET ";
 	$query .= "databaseField1 = '$updatedField1', databaseField1 = '$updatedField2' ";
 	$query .= "WHERE counter = {$counter}";
 	$result = mysqli_query($connection, $query);
